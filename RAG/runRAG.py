9a5fc10b1cd54284c245e414d27b0c942f5a1b0f
Rigved <rigvedmanoj1998@gmail.com>
2024-03-18 21:02:17 -0400

    integrated code

--- RAG/runRAG.py
+++ RAG/runRAG.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import psycopg2
 from RAG import retrieve_documents, generate_text
-from JobScraper import dbFunctions as db
-import numpy as np
+from configparser import ConfigParser
+
+
+def config(filename='dbConfig.config', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+    return {param[0]: param[1] for param in parser.items(section)}
+
 
 def getdata():
-    conn = psycopg2.connect(
-        dbname="postgres",
-        user="postgres",
-        password="rigved",
-        host="localhost",
-        port="5432"
-    )
+    params = config()
+    conn = psycopg2.connect(**params)
     sql_query = "SELECT * FROM jobs;"
     df = pd.read_sql_query(sql_query, conn)
     conn.close()
@@ -20,11 +21,13 @@ def getdata():
 
 
 job_data = getdata()
-job_data['info'] = job_data.apply(lambda row: 'Company: ' + row['company'] + '\nJob Title: ' + row['jobtitle'] + '\nJob Description: ' + row['jobdetails'] + '\nLink to the Job: ' + row['link'], axis=1)
-query = "give me a summary of the top 5 data engineer jobs that uses python along with the url"
+job_data['info'] = job_data.apply(
+    lambda row: 'Company: ' + row['company'] + '\nJob Title: ' + row['jobtitle'] + '\nJob Description: ' + row[
+        'jobdetails'] + '\nLink to the Job: ' + row['link'], axis=1)
+query = "give me a job that has data pipelines, scala and python. All 3 must be there"
 relevant_doc = retrieve_documents(query, job_data['info'])
 print("Query:", query)
-print("Most Relevant Document:", relevant_doc)
+#print("Most Relevant Document:", relevant_doc)
 relevant_doc = '\n'.join(relevant_doc)
 prompt = """
 DOCUMENT: {}
@@ -34,11 +37,14 @@ Answer the user's QUESTION using the DOCUMENT text above.
 Keep your answer grounded in the facts of the DOCUMENT.
 If the DOCUMENT doesn’t contain the facts to answer the QUESTION, return {{NONE}}
 """.format(relevant_doc, query)
-# If the DOCUMENT doesn’t contain the facts to answer the QUESTION, return {{NONE}}
+
+
 # Your OpenAI API key
 api_key = ''
+
 # Endpoint for ChatGPT v1 API
 endpoint = 'https://api.openai.com/v1/chat/completions'
+
 # Generate text
 generated_text = generate_text(prompt, api_key, endpoint)
 print("Generated Text:")
